f014c6cbd3c7352036b72d8836b3fbb7aa0f83db
guilhermeUpToTask <guilherme256teretelteltel@gmail.com>
2024-03-29 12:54:27 -0300

    docker compose integrate

--- mycronjob.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# This is my cron job script
-* * * * * /usr/local/bin/python3 /app/test_req.py
---
+++ scrapy_cron.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This is my cron job script
+2 * * * * cd /app/mercado_livre && /usr/local/bin/scrapy crawl ocultismo
