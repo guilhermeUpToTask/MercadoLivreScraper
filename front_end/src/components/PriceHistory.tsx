97af72938e254e3e9655a13110950db09991d832
guilhermeUpToTask <guilherme256teretelteltel@gmail.com>
2024-04-26 10:59:46 -0300

    tremor charts implemented

--- front_end/src/components/PriceHistory.tsx
+++ front_end/src/components/PriceHistory.tsx
@@ -1,26 +1,43 @@
 import React from "react";
 import usePriceHistoryQuery from "../hooks/usePriceHistoryQuery";
+import { AreaChart } from '@tremor/react';
+import { PriceHistoryType } from "../lib/types/types";
 
 interface IPriceHistoryProps {
     product_id: number;
 }
 
+
+
+const dataFormatter = (number: number) =>
+    `$${Intl.NumberFormat('br').format(number).toString()}`;
+
+
 export default function PriceHistory(props: IPriceHistoryProps): React.ReactElement {
     const { data: prices, error } = usePriceHistoryQuery(props.product_id);
 
-    const PriceList = () => {
-        if (error) return <p>Error: {error.message}</p>
-
-        if (!prices) return <p>Loading...</p>
-
-        return prices.map((price) => {
-            return <p key={price.price_date}>price: {price.price} date: {price.price_date}</p>
-        })
+    const pricesFormatter = (prices: PriceHistoryType[]) => {
+        return prices.map((item: PriceHistoryType) => ({
+            price: item.price,
+            date: new Date(item.price_date).toLocaleDateString("en-US", { month: 'short', day: 'numeric' }),
+        }));
     }
-    
+
+    if (error) return <p>Error fetching prices</p>;
+
     return (
         <section className="max-w-4xl mx-auto mt-2">
-            <PriceList/>
+            {prices && <AreaChart
+                className="h-80"
+                data={pricesFormatter(prices)}
+                index="date"
+                categories={['price']}
+                colors={['yellow']}
+                valueFormatter={dataFormatter}
+                yAxisWidth={60}
+                onValueChange={(v) => console.log(v)}
+            />}
+
         </section>
-    )
+    );
 }
